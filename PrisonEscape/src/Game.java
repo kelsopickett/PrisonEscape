81ae99d6b4b22344c24f8bdfa3fbaa7360f4c5d7
Kelso Pickett <kelseypickett@gmail.com>
2016-05-16 16:49:44 -0700

    Reorganised code; actual game is now broken out into its own method.

--- PrisonEscape/src/Game.java
+++ PrisonEscape/src/Game.java
@@ -2,24 +2,43 @@
 public class Game {
 
 	public static void main(String[] args) {
+		
 		// TODO Start menu, only option is "Start a New Game"
+		newGame();
+	}
+
+	private static void newGame() {
 		// TODO Prompt user to enter character name
 		// TODO Display opening "crawl" with backstory
 		// TODO Display first scene (prison cell)
 		// TODO Get user choice for first scene
-		// TODO If the user advances, display second scene (int. hallway outside cell); else, take action based on user choice
+		/* TODO If the user advances, display second scene (int. hallway outside cell)
+		 * else, take action based on user choice
+		 */
 		// TODO Get user choice for second scene
-		// TODO If the user advances, display the third scene (cafeteria); else, take action based on user choice
+		/* TODO If the user advances, display the third scene (cafeteria)
+		 * else, take action based on user choice
+		 */
 		// TODO Get user choice for third scene
-		// TODO If the user advances, display the fourth scene (kitchen); else, take action based on user choice
+		/* TODO If the user advances, display the fourth scene (kitchen)
+		 * else, take action based on user choice
+		 */
 		// TODO Get user choice for fourth scene
-		// TODO If the user advances, display the fifth scene (ext. yard); else, take action based on user choice
+		/* TODO If the user advances, display the fifth scene (ext. yard)
+		 * else, take action based on user choice
+		 */
 		// TODO Get user choice for fifth scene
-		// TODO If the user advances, display the sixth scene (infirmary); else, take action based on user choice
+		/* TODO If the user advances, display the sixth scene (infirmary)
+		 * else, take action based on user choice
+		 */
 		// TODO Get user choice for sixth scene
-		// TODO If the user advances, display the seventh scene (ext. front yard); else, take action based on user choice
+		/* TODO If the user advances, display the seventh scene (ext. front yard)
+		 * else, take action based on user choice
+		 */
 		// TODO Get user choice for seventh scene
-		// TODO If the user advances, display the eighth scene and score (ext. outside prison walls); else, take action based on user choice
+		/* TODO If the user advances, display the eighth scene and score (ext. outside prison walls)
+		 * else, take action based on user choice
+		 */
 	}
 
 }
