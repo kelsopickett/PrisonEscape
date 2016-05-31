bdf3e6ec890f47ea3d13effbcad12f1e88a5e2d7
Kelso Pickett <kelseypickett@gmail.com>
2016-05-31 16:50:29 -0700

    Added constructor for final scene, which has no user choices.

--- PrisonEscape/src/Scene.java
+++ PrisonEscape/src/Scene.java
@@ -1,22 +1,35 @@
 public class Scene {
 	
-	public String mDescription;
+	public String description;
 	public int cigsAvail;
 	public String choiceA;
 	public String choiceB;
 	public String choiceC;
 	public String choiceD;	
+	public String resultA;
+	public String resultB;
+	public String resultC;
+	public String resultD;
 
-	public Scene(String description, int cigsAvail, String choiceA, String choiceB, String choiceC, String choiceD) {
-		this.mDescription = description;
+	public Scene(String description, int cigsAvail, String choiceA, String choiceB, String choiceC, 
+			String choiceD, String resultA, String resultB, String resultC, String resultD) {
+		this.description = description;
 		this.cigsAvail = cigsAvail;
 		this.choiceA = choiceA;
 		this.choiceB = choiceB;
 		this.choiceC = choiceC;
 		this.choiceD = choiceD;
+		this.resultA = resultA;
+		this.resultB = resultB;
+		this.resultC = resultC;
+		this.resultD = resultD;
 	}
 	
-	public Scene(String description) {
-		mDescription = description;
+	public Scene(String description, String choiceA, String choiceB, String choiceC, String choiceD) {
+		this.description = description;
+		this.choiceA = choiceA;
+		this.choiceB = choiceB;
+		this.choiceC = choiceC;
+		this.choiceD = choiceD;
 	}
 }
